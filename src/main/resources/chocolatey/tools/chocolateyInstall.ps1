f6942754a1f435017e7cdfbb2e99a43a46977d03
tayloryork <tayloryork@gmail.com>
2014-07-18 14:30:23 -0500

    Adding tomcat dependency and updating install.

--- src/main/resources/chocolatey/tools/chocolateyInstall.ps1
+++ src/main/resources/chocolatey/tools/chocolateyInstall.ps1
@@ -1,1 +1,14 @@
-Install-ChocolateyPackage '${artifactId}' 'http://repo01.dev.e2open.com:8080/archiva/repository/e2open.dev/com/e2open/e2ui/framework/1.9/framework-1.9.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+try {
+	Write-ChocolateySuccess 'MyPackage!!!!!!!!!!'
+	$version = "1.9"
+	$scriptPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+	$savePath = "$scriptPath\framework-$version.zip"
+	$appName = "taylorsApp"
+	Write-Debug "SavePath: $savePath"
+	
+	Get-ChocolateyWebFile $appName $savePath "http://repo01.dev.e2open.com:8080/archiva/repository/e2open.dev/com/e2open/e2ui/framework/$version/framework-$version.zip"
+    
+} catch {
+  Write-ChocolateyFailure $appName $($_.Exception.Message)
+  throw
+}
